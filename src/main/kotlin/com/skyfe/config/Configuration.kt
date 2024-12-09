67d916f7e188b4ba4c46277f9a28819e19abfe34
sitranto <anatoliyl2006@gmail.com>
2024-12-10 02:21:44 +0300

    Added configurations

---
+++ src/main/kotlin/com/skyfe/config/Configuration.kt
@@ -0,0 +1,39 @@
+package com.skyfe.config
+
+import com.skyfe.repository.UserRepository
+import com.skyfe.service.CustomUserDetailsService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
+@Configuration
+@EnableWebSecurity
+class Configuration {
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService = //используем userDetailsService, который упрощает нам работу с Dao, описан в кастомном классе в сервисах
+        CustomUserDetailsService(userRepository)
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = // Шифруем пароли для проверки в Dao
+        BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider =
+        DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepository))
+                it.setPasswordEncoder(passwordEncoder())
+            }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
+        config.authenticationManager
+}
---
+++ src/main/kotlin/com/skyfe/config/SecurityConfiguration.kt
@@ -0,0 +1,27 @@
+package com.skyfe.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.DefaultSecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(private val authenticationProvider: AuthenticationProvider) {
+    @Bean
+    fun filterChain(http: HttpSecurity): DefaultSecurityFilterChain =
+        http
+            .authorizeHttpRequests {
+                it
+                    .anyRequest().permitAll()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .build()
+}
