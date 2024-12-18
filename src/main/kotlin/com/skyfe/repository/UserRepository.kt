ba977ba2b63e9bc9ffe8f279387549448cc86e40
sitranto <anatoliyl2006@gmail.com>
2024-12-19 02:35:17 +0300

    registration validation

--- src/main/kotlin/com/skyfe/repository/UserRepository.kt
+++ src/main/kotlin/com/skyfe/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package com.skyfe.repository
 import com.skyfe.domain.model.User
 
 interface UserRepository: BaseRepository<User> {
-    fun findByUsername(username: String): User
+    fun existsByUsername(username: String): Boolean
     fun findByNumber(number: String): User
+    fun existsByNumber(number: String): Boolean
 }
