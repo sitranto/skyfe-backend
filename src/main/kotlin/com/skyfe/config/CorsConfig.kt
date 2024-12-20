406b0d9122021ecf8ca1e2289d6b1291ae3fc18a
sitranto <anatoliyl2006@gmail.com>
2024-12-20 04:40:45 +0300

    cors disable

--- src/main/kotlin/com/skyfe/config/CorsConfig.kt
+++ src/main/kotlin/com/skyfe/config/CorsConfig.kt
@@ -1,11 +1,13 @@
 package com.skyfe.config
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @org.springframework.context.annotation.Configuration
+@EnableWebMvc
 class CorsConfig: WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedMethods("*")
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
     }
 }
--- src/main/kotlin/com/skyfe/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/skyfe/config/SecurityConfiguration.kt
@@ -1,20 +1,14 @@
 package com.skyfe.config
 
-import jakarta.servlet.http.HttpServletRequest
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebSecurity
@@ -26,10 +20,11 @@ class SecurityConfiguration(private val authenticationProvider: AuthenticationPr
     ): DefaultSecurityFilterChain =
         http
             .csrf().disable()
+            .cors().disable()
             .authorizeHttpRequests {
                 it
                     .requestMatchers("/api/auth", "/api/user", "/api/user/number", "/api/user/username").permitAll()
-                    .requestMatchers("/api/**").fullyAuthenticated()
+                    .requestMatchers("/api/**").authenticated()
             }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
