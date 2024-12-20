d56b9ef678c21c12683c06de95d1dfb5e5ea1712
sitranto <anatoliyl2006@gmail.com>
2024-12-20 04:57:34 +0300

    getting messages

--- src/main/kotlin/com/skyfe/controller/MessageController.kt
+++ src/main/kotlin/com/skyfe/controller/MessageController.kt
@@ -3,10 +3,13 @@ package com.skyfe.controller
 import com.skyfe.domain.model.Message
 import com.skyfe.service.MessageService
 import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/message")
 @RequiredArgsConstructor
 class MessageController(private val messageService: MessageService) {
+    @GetMapping
+    fun getAllMessages(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) token: String,
+        @RequestHeader("chatId") chatId: Long) =
+        messageService.getAllMessages(token, chatId)
     @PostMapping("/send")
     fun sendMessage(@RequestBody payload: Message) =
         messageService.sendMessage(payload)
--- src/main/kotlin/com/skyfe/service/MessageService.kt
+++ src/main/kotlin/com/skyfe/service/MessageService.kt
@@ -1,11 +1,30 @@
 package com.skyfe.service
 
 import com.skyfe.domain.model.Message
+import com.skyfe.middlewhare.DataNotFoundException
+import com.skyfe.repository.ChatRepository
 import com.skyfe.repository.MessageRepository
+import com.skyfe.repository.UserRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
 @Service
-class MessageService(private val messageRepository: MessageRepository) {
+class MessageService(
+    private val messageRepository: MessageRepository,
+    private val jwtService: JwtService,
+    private val userRepository: UserRepository,
+    private val chatRepository: ChatRepository
+    ) {
+    fun getAllMessages(token: String, chatId: Long): List<Message> {
+        val number = jwtService.extractNumber(token.substringAfter("Bearer ")).toString()
+        val user = userRepository.findByNumber(number)
+        for (i in user.userChats) {
+            if (i.chat.id == chatId) {
+                return chatRepository.findById(chatId.toInt()).get().messages
+            }
+        }
+        throw DataNotFoundException(HttpStatus.FORBIDDEN, "No access to chat")
+    }
 
     fun sendMessage(message: Message): Message =
         messageRepository.save(message)
